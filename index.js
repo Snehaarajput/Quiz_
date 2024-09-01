7a84585202f0ac533383651bf1c3d5989870eb45
Snehaarajput <138353051+Snehaarajput@users.noreply.github.com>
2024-09-02 04:29:49 +0530

    Update index.js
    change database connection field

--- index.js
+++ index.js
@@ -8,12 +8,33 @@ const quizRoutes = require('./routes/Quiz');
 
 const app = express();
 const port = process.env.PORT || 3000;
-const mongoURI = process.env.MONGO_URI || 'mongodb://localhost:27017/quizee';
+const mongoURI = process.env.MONGO_URI || "mongodb+srv://rajputsnehaa8:9BCwXYLmy6KIXphD@cluster0.vocys.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
-// Database Connection
-mongoose.connect(mongoURI)
-    .then(() => console.log('Connected to MongoDB'))
-    .catch(err => console.error('Could not connect to MongoDB', err));
+// // Database Connection
+// mongoose.connect(mongoURI)
+//     .then(() => console.log('Connected to MongoDB'))
+//     .catch(err => console.error('Could not connect to MongoDB', err));
+
+const client = new MongoClient(mongoURI, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
 
 
 // Middleware
